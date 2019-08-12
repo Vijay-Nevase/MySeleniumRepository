b1beb49972426ce986fa07888636232b7fdfac7c
Vijay <nvijay.vn29@gmail.com>
2019-08-12 21:36:02 +0530

    Sprint2

---
+++ FirstJavaProject/src/opps/abstraction/Sprint2.java
@@ -0,0 +1,18 @@
+package opps.abstraction;
+
+public abstract class Sprint2 extends Sprint1{
+
+	
+	public void Delete() {
+		System.out.println("Delete");
+		
+	}
+
+
+	public void update() {
+		System.out.println("Update");		
+	}
+
+	
+
+}
