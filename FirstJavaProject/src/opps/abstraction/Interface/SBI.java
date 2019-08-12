24d1eaf0ec214538a7f5e308b8a0cd8d1831a723
Vijay <nvijay.vn29@gmail.com>
2019-08-12 21:33:20 +0530

    Ok

---
+++ FirstJavaProject/src/opps/abstraction/Interface/SBI.java
@@ -0,0 +1,24 @@
+package opps.abstraction.Interface;
+
+public class SBI implements RBI{
+	
+	public void savingAccount() {
+		System.out.println("Saving In SBI");
+		
+	}
+
+	public void currentAccount() {
+		System.out.println("Current In SBI");
+		
+	}
+
+	public void debitCard() {
+		System.out.println("Debit card In SBI");
+		
+	}
+
+	public void creditCards() {
+		// TODO Auto-generated method stub
+		System.out.println("Credit In SBI");
+	}
+}
