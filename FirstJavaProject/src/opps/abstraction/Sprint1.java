81180f6c7a52d28c9774d88f092bde2a8820cf9b
Vijay <nvijay.vn29@gmail.com>
2019-08-12 20:52:02 +0530

    ok

---
+++ FirstJavaProject/src/opps/abstraction/Sprint1.java
@@ -0,0 +1,19 @@
+package opps.abstraction;
+
+public abstract class Sprint1 extends ExcelApplication{
+
+	
+	public void spreadsheet() {
+		System.out.println("Spreed Sheet");
+		
+	}
+
+	
+	public void save() {
+		System.out.println("Save");
+		
+	}
+
+	
+
+}
